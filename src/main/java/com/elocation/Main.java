ad1fbacc190298084b0823514473a221dd7a6186
ahmed ksentini <ksentiniahmed1994@gmail.com>
2017-09-15 16:43:17 +0100

    intialize

---
+++ src/main/java/com/elocation/Main.java
@@ -0,0 +1,14 @@
+package com.elocation;
+/**
+ * Created by ahmedks on 15/09/17.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main (String[] args){
+        SpringApplication.run(Main.class, args);
+
+    }
+}
